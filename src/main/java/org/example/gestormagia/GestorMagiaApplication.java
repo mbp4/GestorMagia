f6846e9373e6181591c8389457f7d59594d71dba
mbp4 <mblanpon@myuax.com>
2024-10-21 11:45:07 +0200

    Initial commit

---
+++ src/main/java/org/example/gestormagia/GestorMagiaApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestormagia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorMagiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorMagiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gestormagia/GestorMagiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gestormagia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorMagiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
