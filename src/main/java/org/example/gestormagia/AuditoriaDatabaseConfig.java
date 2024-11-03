8e935adad759040ca553a0f79049aaa3a54cf2d1
mbp4 <mblanpon@myuax.com>
2024-11-03 13:43:01 +0100

    arreglos

--- src/main/java/org/example/gestormagia/AuditoriaDatabaseConfig.java
+++ src/main/java/org/example/gestormagia/AuditoriaDatabaseConfig.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class AuditoriaDatabaseConfig {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/auditorias?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:8889/hechizos?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
