1ee2c5cdf0b5f9eaedfd2bf0c88a0fac0c690433
mbp4 <mblanpon@myuax.com>
2024-11-03 13:49:50 +0100

    arreglos

--- src/main/java/org/example/gestormagia/servicios/EventoMagicoServicio.java
+++ src/main/java/org/example/gestormagia/servicios/EventoMagicoServicio.java
@@ -1,7 +1,9 @@
 package org.example.gestormagia.servicios;
 
 import org.example.gestormagia.DatabaseConfig;
+import org.example.gestormagia.model.dao.EventoMagicoDao;
 import org.example.gestormagia.model.entidades.EventoMagico;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
@@ -14,47 +16,23 @@ import java.util.List;
 @Service
 public class EventoMagicoServicio {
 
-    public List<EventoMagico> listadoEventos() {
-        List<EventoMagico> eventos = new ArrayList<>();
-        String query = "SELECT * FROM eventosMagicos";
+    @Autowired
+    private EventoMagicoDao eventoMagicoDao;
 
-        try (Connection connection = DatabaseConfig.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery()) {
-
-            while (resultSet.next()) {
-                EventoMagico evento = new EventoMagico();
-                // Set properties of evento from resultSet
-                eventos.add(evento);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+    public List<EventoMagico> listadoEventos(){
+        List<EventoMagico> eventos = eventoMagicoDao.findAll();
         return eventos;
     }
 
-    public int guardarEvento(EventoMagico eventoMagico) {
+    public int guardarEvento(EventoMagico eventoMagico){
         int result = 0;
-        String query = "INSERT INTO eventosMagicos (mago, tipo) VALUES (?, ?)";
-
-        try (Connection connection = DatabaseConfig.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-
-            statement.setString(1, eventoMagico.getMago());
-            statement.setString(2, eventoMagico.getTipo());
-
-            if (eventoMagico.getMago().equals("Voldemort")) {
-                result = 1;
-            } else if (eventoMagico.getTipo().equals("Pocima")) {
-                result = 2;
-            }
-
-            if (result == 0) {
-                statement.executeUpdate();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        if (eventoMagico.getMago().equals("Voldemort")){
+            result = 1;
+        }else if (eventoMagico.getTipo().equals("Pocima")){
+            result = 2;
+        }
+        if (result == 0) {
+            eventoMagicoDao.save(eventoMagico);
         }
 
         return result;
